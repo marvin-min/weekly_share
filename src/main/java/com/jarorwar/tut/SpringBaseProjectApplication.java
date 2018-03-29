f3678ee5aa07901982ed29a3a4564e99e1258b1f
marvin-min <xiaorong.min@gmail.com>
2018-03-29 21:30:20 +0800

    init

---
+++ src/main/java/com/jarorwar/tut/SpringBaseProjectApplication.java
@@ -0,0 +1,12 @@
+package com.jarorwar.tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBaseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBaseProjectApplication.class, args);
+	}
+}
